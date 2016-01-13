1ac56685d9f4d2bf079e7f02ff1c1504bde92a73
Brandon Feinstein <brandon.feinstein@Hotmail.com>
2016-01-12 17:26:17 -0800

    Enclosed contents of each script in an IIFE.

--- js/portfolio_view.js
+++ js/portfolio_view.js
@@ -1,106 +1,109 @@
 // Configure a view object, to hold all our functions for dynamic updates and article-related event handlers.
-var articleView = {};
+(function(module) {
+  var articleView = {};
 
-articleView.populateFilters = function() {
-  $('article').each(function() {
-    if (!$(this).hasClass('template')) {
-      var val = $(this).find('address a').text();
-      var optionTag = '<option value="' + val + '">' + val + '</option>';
-      $('#author-filter').append(optionTag);
+  articleView.populateFilters = function() {
+    $('article').each(function() {
+      if (!$(this).hasClass('template')) {
+        var val = $(this).find('address a').text();
+        var optionTag = '<option value="' + val + '">' + val + '</option>';
+        $('#author-filter').append(optionTag);
 
-      val = $(this).attr('data-category');
-      optionTag = '<option value="' + val + '">' + val + '</option>';
-      if ($('#category-filter option[value="' + val + '"]').length === 0) {
-        $('#category-filter').append(optionTag);
+        val = $(this).attr('data-category');
+        optionTag = '<option value="' + val + '">' + val + '</option>';
+        if ($('#category-filter option[value="' + val + '"]').length === 0) {
+          $('#category-filter').append(optionTag);
+        }
       }
-    }
-  });
-};
-
-articleView.handleCategoryFilter = function() {
-  $('#category-filter').on('change', function() {
-    if ($(this).val()) {
-      $('article').hide();
-      $('article[data-category="' + $(this).val() + '"]').fadeIn();
-    } else {
-      $('article').fadeIn();
-      $('article.template').hide();
-    }
-    $('#author-filter').val('');
-  });
-};
-
-articleView.handleMainNav = function() {
-
-  $('.main-nav .tab').on('click', function(event) {
-    var $targetData = $(this).data('content');
-    $('.tab-content').each(function() {
-      if($(this).attr('id') === $targetData) {
-        $(this).show();
-      } else {
-        $(this).hide();
-      };
     });
-  });
+  };
 
-  $('.main-nav .tab:first').click(); // Let's now trigger a click on the first .tab element, to set up the page.
-};
+  articleView.handleCategoryFilter = function() {
+    $('#category-filter').on('change', function() {
+      if ($(this).val()) {
+        $('article').hide();
+        $('article[data-category="' + $(this).val() + '"]').fadeIn();
+      } else {
+        $('article').fadeIn();
+        $('article.template').hide();
+      }
+      $('#author-filter').val('');
+    });
+  };
 
-articleView.setTeasers = function() {
-  $('.article-body *:nth-of-type(n+2)').hide(); // Hide elements beyond the first 2 in any artcile body.
+  articleView.handleMainNav = function() {
 
-  $('.read-on').on('click', function(e){
-    e.preventDefault();
-    $(this).siblings('.article-body').children().show();
-    $(this).hide();
-  });
+    $('.main-nav .tab').on('click', function(event) {
+      var $targetData = $(this).data('content');
+      $('.tab-content').each(function() {
+        if($(this).attr('id') === $targetData) {
+          $(this).show();
+        } else {
+          $(this).hide();
+        };
+      });
+    });
 
-};
-articleView.initNewArticlePage = function() {
-  $('.tab-content').show();
-  $('#export-field').hide();
-  $('#article-json').on('focus', function(){
-    this.select();
-  });
+    $('.main-nav .tab:first').click(); // Let's now trigger a click on the first .tab element, to set up the page.
+  };
 
-  $('#new-form').on('change', 'input, textarea', articleView.create);
-};
+  articleView.setTeasers = function() {
+    $('.article-body *:nth-of-type(n+2)').hide(); // Hide elements beyond the first 2 in any artcile body.
 
-articleView.create = function() {
-  var article;
-  $('#articles').empty();
+    $('.read-on').on('click', function(e){
+      e.preventDefault();
+      $(this).siblings('.article-body').children().show();
+      $(this).hide();
+    });
 
-  // Instantiate an article based on what's in the form fields:
-  article = new Article({
-    title: $('#article-title').val(),
-    author: $('#article-author').val(),
-    authorUrl: $('#article-author-url').val(),
-    category: $('#article-category').val(),
-    body: $('#article-body').val(),
-    publishedOn: $('#article-published:checked').length ? util.today() : null
-  });
+  };
+  articleView.initNewArticlePage = function() {
+    $('.tab-content').show();
+    $('#export-field').hide();
+    $('#article-json').on('focus', function(){
+      this.select();
+    });
 
-  // Use the Handblebars template to put this new article into the DOM:
-  $('#articles').append(article.toHtml());
+    $('#new-form').on('change', 'input, textarea', articleView.create);
+  };
 
-  // Activate the highlighting of any code blocks:
-  $('pre code').each(function(i, block) {
-    hljs.highlightBlock(block);
-  });
+  articleView.create = function() {
+    var article;
+    $('#articles').empty();
 
-  // Export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
-  $('#export-field').show();
-  $('#article-json').val(JSON.stringify(article) + ',');
-};
+    // Instantiate an article based on what's in the form fields:
+    article = new Article({
+      title: $('#article-title').val(),
+      author: $('#article-author').val(),
+      authorUrl: $('#article-author-url').val(),
+      category: $('#article-category').val(),
+      body: $('#article-body').val(),
+      publishedOn: $('#article-published:checked').length ? util.today() : null
+    });
+
+    // Use the Handblebars template to put this new article into the DOM:
+    $('#articles').append(article.toHtml());
+
+    // Activate the highlighting of any code blocks:
+    $('pre code').each(function(i, block) {
+      hljs.highlightBlock(block);
+    });
+
+    // Export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
+    $('#export-field').show();
+    $('#article-json').val(JSON.stringify(article) + ',');
+  };
 
 
-articleView.initIndexPage = function() {
-  Article.all.forEach(function(a){
-    $('#articles').append(a.toHtml());
-  });
+  articleView.initIndexPage = function() {
+    Article.all.forEach(function(a){
+      $('#articles').append(a.toHtml());
+    });
 
-  articleView.populateFilters();
-  articleView.handleCategoryFilter();
-  articleView.handleMainNav();
-  articleView.setTeasers();
-};
+    articleView.populateFilters();
+    articleView.handleCategoryFilter();
+    articleView.handleMainNav();
+    articleView.setTeasers();
+  };
+  module.articleView = articleView;
+})(window);
