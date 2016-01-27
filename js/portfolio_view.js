d74130a0db364f4066986d7cb115518381549a7a
Brandon Feinstein <brandon.feinstein@Hotmail.com>
2016-01-26 22:27:00 -0800

    commented portfollioview.js

--- js/portfolio_view.js
+++ js/portfolio_view.js
@@ -1,30 +1,19 @@
-(function(module) {
-  var articleView = {};
+(function(module) { //IIFE
+  var articleView = {}; // articleView object
 
   articleView.setTeasers = function() {
     $('.article-body *:nth-of-type(n+2)').hide(); // Hide elements beyond the first 2 in any artcile body.
 
-    $('.read-on').on('click', function(e){
+    $('.read-on').on('click', function(e){ // shows full article body when read on is clicked
       e.preventDefault();
       $(this).siblings('.article-body').children().show();
       $(this).hide();
     });
-
-  };
-  articleView.initNewArticlePage = function() {
-    $('.tab-content').show();
-    $('#export-field').hide();
-    $('#article-json').on('focus', function(){
-      this.select();
-    });
-
-    $('#new-form').on('change', 'input, textarea', articleView.create);
   };
 
   articleView.create = function() {
     var article;
-    $('#articles').empty();
-
+    $('#articles').empty(); //empty anything in the aritcles section.
     // Instantiate an article based on what's in the form fields:
     article = new Article({
       title: $('#article-title').val(),
@@ -33,29 +22,16 @@
       category: $('#article-category').val(),
       body: $('#article-body').val(),
       img: $('#article-img').val(),
-      publishedOn: $('#article-published:checked').length ? util.today() : null
     });
-
     // Use the Handblebars template to put this new article into the DOM:
     $('#articles').append(article.toHtml());
-
-    // Activate the highlighting of any code blocks:
-    $('pre code').each(function(i, block) {
-      hljs.highlightBlock(block);
-    });
-
-    // Export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
-    $('#export-field').show();
-    $('#article-json').val(JSON.stringify(article) + ',');
   };
-
 
   articleView.initIndexPage = function() {
-    Article.all.forEach(function(a){
+    Article.all.forEach(function(a){  //appends each article to the articles section
       $('#articles').append(a.toHtml());
     });
-
-    articleView.setTeasers();
+    articleView.setTeasers(); 
   };
-  module.articleView = articleView;
+  module.articleView = articleView; //attaches articleView to window object
 })(window);
