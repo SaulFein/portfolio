d3d1cbf98f6e10fab19a806da098dd95eb9b4272
Brandon Feinstein <brandon.feinstein@Hotmail.com>
2016-01-28 06:31:06 -0800

    added some more comments to repoview.js

--- js/repoView.js
+++ js/repoView.js
@@ -6,12 +6,12 @@
     $about.find('ul').empty();
   };
 
-  var render = function(repo) {
+  var render = function(repo) {// render function creas a new li for each repo and creats a link with the html_url from the github apit and repo full_name attribute for the link text.
     return $('<li>')
     .html('<a href="' + repo.html_url + '">' + repo.full_name + '</a>');
   };
 
-  repoView.index = function() {
+  repoView.index = function() { //empties #about section 'ul's with ui function and then appends repos with stargazers_count to the  DOM in the #repolist section.
     ui();
     $('#repolist').append(
       repos.with('stargazers_count').map(render) //appends repos with a stargazers_count to repolist by running the render function for each repo.
