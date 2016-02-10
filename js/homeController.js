bd3f381026a93b67af2866d6fb9ec5d720783288
Brandon Feinstein <brandon.feinstein@Hotmail.com>
2016-02-10 14:50:16 -0800

    added underslider to homepage.

--- js/homeController.js
+++ js/homeController.js
@@ -2,30 +2,10 @@
   var homeController = {};
 
   homeController.index = function() {
-    if(!$('#slide')[0]) {
-      Project.fetchAll();
-      Project.all.forEach(function(a){  //appends each project to the projects section
-        $('#home-page').append(a.toHtml2());
-      });
-      $('#projects').hide();
-      $('#about').hide();
-    } else {
-      $('#home-page').show();
-      $('#projects').hide();
-      $('#about').hide();
-    };
-    // $('#home-page > article:gt(0)').hide();
-    //
-    // setInterval(function() {
-    //   $('#home-page > article:first')
-    //     .fadeOut(1000)
-    //     .next()
-    //     .fadeIn(1000)
-    //     .end()
-    //     .appendTo('#home-page');
-    // }, 3000);
+    $('#projects').hide();
+    $('#about').hide();
+    $('.unslider').show();
+    $('#home-page').show();
   };
-
-
   module.homeController = homeController;
 })(window);
