73d649e76197fb5b324bfcaf6a875089d2f7f7d1
Brandon Feinstein <brandon.feinstein@Hotmail.com>
2016-02-13 13:51:12 -0800

    changed mobile menu style and worked on other mobile styling

---
+++ js/extrafeature.js
@@ -0,0 +1,28 @@
+$(document).ready(function($) {
+  $('.my-slider').unslider({
+    autoplay: true,
+    infinite: true,
+    delay: 3000,
+    nav: false,
+    arrows: false,
+  });
+});
+
+$('#ham-menu').click(function(){
+  // $('.list-menu').toggle();
+  $('#ham-menu').toggleClass('icon-menu icon-cross');
+  $('.icon-cross').click(function(){
+    $('.list-menu').hide();
+  });
+  $('.icon-menu').click(function(){
+    $('.list-menu').show();
+  });
+
+});
+
+
+//
+// $('#click_advance').click(function(){
+//   $('#display_advance').toggle('1000');
+//   $("i",this).toggleClass("icon-circle-arrow-up icon-circle-arrow-down");
+// });
