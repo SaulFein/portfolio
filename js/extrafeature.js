a2bc8690bcef7bb405081f4cbb2fb32e50b9ce18
Brandon Feinstein <brandon.feinstein@Hotmail.com>
2016-02-24 12:06:47 -0800

    cleaned up CSS.

--- js/extrafeature.js
+++ js/extrafeature.js
@@ -9,34 +9,14 @@ $(document).ready(function($) {
 });
 
 $('#ham-menu').click(function(){
-  // $('.list-menu').toggle();
   $('#ham-menu').toggleClass('icon-menu icon-cross');
   $('#list-menu').toggleClass('lm-height lm-close');
-
-  // $('.list-menu').toggleClass('lm-close');
-
 });
 
 $('.icon-cross').click(function(){
   $('.main-nav ul li').toggleClass('drop');
-  // $('.list-menu').toggleClass('lm-close');
-
 });
+
 $('.icon-menu').click(function(){
   $('.main-nav ul li').toggleClass('drop');
-  // $('.list-menu').toggleClass('lm-height');
-
-
 });
-
-
-
-// $('a').click(function(){
-//   $('nav').toggleClass('drop');
-// })
-
-//
-// $('#click_advance').click(function(){
-//   $('#display_advance').toggle('1000');
-//   $("i",this).toggleClass("icon-circle-arrow-up icon-circle-arrow-down");
-// });
